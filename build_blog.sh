59bb7872157043fc7fec653ae2ae8340f9cde862
bernardo <bernardo.stearns>
2024-09-30 20:56:11 +0100

    rendering every 30s

--- build_blog.sh
+++ build_blog.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 source .env
-TARGET_DIR=personal-notes
-git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/berstearns/personal-notes $TARGET_DIR
-pip install -r personal-notes/blog/requirements.txt
-python3 personal-notes/blog/render.py
+while 1;do
+    echo 'sleeping for 30 seconds'
+    sleep 30
+    echo 'rendering'
+    TARGET_DIR=personal-notes
+    git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/berstearns/personal-notes $TARGET_DIR
+    pip install -r personal-notes/blog/requirements.txt
+    python3 personal-notes/blog/render.py
+    clear
+done
