d79ee4a83a3a4b163e0b819022dfa1a4761a240a
Ubuntu <ubuntu@ip-172-31-32-22.ec2.internal>
2024-09-30 19:57:06 +0000

    manual rendering

--- build_blog.sh
+++ build_blog.sh
@@ -3,5 +3,6 @@
 source .env
 TARGET_DIR=personal-notes
 git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/berstearns/personal-notes $TARGET_DIR
-pip install -r personal-notes/blog/requirements.txt
-python3 personal-notes/blog/render.py
+python3 -m venv venv
+./venv/bin/python -m pip install -r ./personal-notes/blog/requirements.txt
+./venv/bin/python personal-notes/blog/render.py
