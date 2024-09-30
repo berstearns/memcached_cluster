3f7c8ea35b0226bae36db0c7317a7996d1359c86
bernardo <bernardo.stearns>
2024-09-30 20:44:37 +0100

    adding python script part

--- build_blog.sh
+++ build_blog.sh
@@ -3,3 +3,5 @@
 source .env
 TARGET_DIR=personal-notes
 git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/berstearns/personal-notes $TARGET_DIR
+pip install -r personal-notes/blog/requirements.txt
+python3 personal-notes/blog/render.py
