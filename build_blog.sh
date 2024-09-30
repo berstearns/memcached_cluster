244f932b2786a4ca92dfd8c72efe1f48cc1da654
bernardo <bernardo.stearns>
2024-09-30 17:19:59 +0100

    rebasing

--- build_blog.sh
+++ build_blog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source .env
 TARGET_DIR=personal-notes
-git -C $TARGET_DIR pull || git clone https://github.com/berstearns/personal-notes $TARGET_DIR
+git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME@:$GIT_PASS@github.com/berstearns/personal-notes $TARGET_DIR
 
