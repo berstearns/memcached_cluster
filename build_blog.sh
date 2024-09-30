dbd15c9a553bd3e7071f1384af03a5f23115aeca
bernardo <bernardo.stearns>
2024-09-30 18:08:27 +0100

    fixing rebase

--- build_blog.sh
+++ build_blog.sh
@@ -2,4 +2,4 @@
 
 source .env
 TARGET_DIR=personal-notes
-git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME@:$GIT_PASS@github.com/berstearns/personal-notes $TARGET_DIR
+git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/berstearns/personal-notes $TARGET_DIR
