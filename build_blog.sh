2d8254e5150523441d03e49a8804d824742b1661
bernardo <bernardo.stearns>
2024-10-08 21:11:49 +0100

    expanding blog

--- build_blog.sh
+++ build_blog.sh
@@ -9,6 +9,6 @@ while true;do
     git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/berstearns/personal-notes $TARGET_DIR
     python3 -m venv venv
     ./venv/bin/python -m pip install -r ./personal-notes/blog/requirements.txt
-    ./venv/bin/python personal-notes/blog/render.py
+    ./venv/bin/python personal-notes/blog/render.py ./src/templates/blog.html
     clear
 done
