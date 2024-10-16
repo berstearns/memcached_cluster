55ca15a86b7d01e31a42cca225aae012aa2685fb
bernardo <bernardo.stearns>
2024-10-16 18:16:50 +0100

    changes

--- build_blog.sh
+++ build_blog.sh
@@ -3,12 +3,13 @@
 source .env
 while true;do
     echo 'sleeping for 30 seconds'
-    sleep 30
+    sleep 3
     echo 'rendering'
     TARGET_DIR=personal-notes
     git -C $TARGET_DIR pull || git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/berstearns/personal-notes $TARGET_DIR
     python3 -m venv venv
     ./venv/bin/python -m pip install -r ./personal-notes/blog/requirements.txt
-    ./venv/bin/python personal-notes/blog/render.py
-    clear
+    ./venv/bin/python personal-notes/blog/render.py ./src/templates/blog.html
+    # clear
+    exit
 done
