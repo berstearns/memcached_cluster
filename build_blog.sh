779b400700c4128b3de7c989f6909a346c291821
bernardo <bernardo.stearns>
2024-10-19 15:38:14 +0100

    generating dynamic conf

--- build_blog.sh
+++ build_blog.sh
@@ -10,5 +10,6 @@ while true;do
     python3 -m venv venv
     ./venv/bin/python -m pip install -r ./personal-notes/blog/requirements.txt
     ./venv/bin/python personal-notes/blog/render.py ./src/templates/
+    ./venv/bin/python personal-notes/blog/build_docker_conf.py ./blog-docker-conf
     clear
 done
